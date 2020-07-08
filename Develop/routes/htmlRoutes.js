a4dc9898c4dd81411dfbcb982b1e2ff1dcd0426e
Justin Suderman <jsuderman1@gmail.com>
2020-07-08 19:17:25 -0400

    added function html routes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,3 +1,15 @@
 // Dependencies
 
 var path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/note", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+      });
+    
+      app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+      });
+    
+};
